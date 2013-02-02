397d463beb00f4491ba4054aad95923e25bb37bc
ryan <rg.crichton@gmail.com>
2013-02-02 14:12:15 -0600

    Added "Patrick Doctor" test data to mock provider service

--- src/main/java/org/jembi/rhea/mocks/ProfessionalRegsitryMockService.java
+++ src/main/java/org/jembi/rhea/mocks/ProfessionalRegsitryMockService.java
@@ -26,6 +26,8 @@ public class ProfessionalRegsitryMockService {
 		log.info("Returning epid for professional with " + id_type + ": " + id_number + " ...");
 		if (id_number.equals("3525410")) {
 			return "e8597a14-436f-1031-8b61-8d373bf4f88f";
+		} else if (id_number.equals("8103869")) {
+			return "2572edfe-5f59-1031-9435-7f3016bad837";
 		} else {
 			return UUID.randomUUID().toString();
 		}
@@ -39,6 +41,8 @@ public class ProfessionalRegsitryMockService {
 		log.info("Returning NID for professional with " + id_type + ": " + id_number + " ...");
 		if (id_number.equals("e8597a14-436f-1031-8b61-8d373bf4f88f")) {
 			return "3525410";
+		} else if (id_number.equals("2572edfe-5f59-1031-9435-7f3016bad837")) {
+			return "8103869";
 		} else {
 			return "0000000000000000";
 		}
